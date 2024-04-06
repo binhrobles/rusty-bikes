5fa8cdd9cd323b7eb1817bc46affb85850f39d14
Binh 🌱 <binhrobles@gmail.com>
2024-04-06 12:48:22 -0400

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
-use tokio::fs;
 use axum::{
-    routing::{get, post},
-    Router,
     extract::Path,
     http::{Method, StatusCode},
+    routing::{get, post},
+    Router,
 };
+use tokio::fs;
+use tower::ServiceBuilder;
 use tower_http::{
     cors::{Any, CorsLayer},
     trace::TraceLayer,
 };
-use tower::ServiceBuilder;
-
 
 #[tokio::main]
 async fn main() {
@@ -25,22 +24,21 @@ async fn main() {
     // TODO: support single GET /v2/directions/:profile spec
     let app = Router::new()
         .route("/heartbeat", get(|| async { "OK" }))
-        .route("/v2/directions/:profile/*result_type", post(directions_handler))
-
+        .route(
+            "/v2/directions/:profile/*result_type",
+            post(directions_handler),
+        )
         // applies a collection of Tower Layers to all of this Router's routes
-        .layer(
-            ServiceBuilder::new()
-                .layer(trace)
-                .layer(cors)
-        );
+        .layer(ServiceBuilder::new().layer(trace).layer(cors));
 
     // run app w/ hyper, bind to 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-// TODO: extract JSON payload
-async fn directions_handler(Path((profile, result_type)): Path<(String, String)>) -> Result<String, StatusCode>  {
+async fn directions_handler(
+    Path((profile, result_type)): Path<(String, String)>,
+) -> Result<String, StatusCode> {
     println!("request of {profile} in {result_type} received");
     if let Ok(contents) = fs::read_to_string("staticGeoJsonResponse.geojson").await {
         Ok(contents)
