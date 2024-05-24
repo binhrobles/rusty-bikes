c4682b8a09fd41c89a695835b4c2d44919e71320
Binh 🌱 <binhrobles@gmail.com>
2024-05-24 10:45:26 -0400

    add handlebars...thanks depcruise!

--- client/.dependency-cruiser.cjs
+++ client/.dependency-cruiser.cjs
@@ -117,7 +117,7 @@ module.exports = {
       from: {},
       to: {
         moreThanOneDependencyType: true,
-        // as it's pretty common to have a type import be a type only import 
+        // as it's pretty common to have a type import be a type only import
         // _and_ (e.g.) a devDependency - don't consider type-only dependency
         // types for this rule
         dependencyTypesNot: ["type-only"]
@@ -125,7 +125,7 @@ module.exports = {
     },
 
     /* rules you might want to tweak for your specific situation: */
-    
+
     {
       name: 'not-to-spec',
       comment:
@@ -216,7 +216,7 @@ module.exports = {
     // includeOnly : [''],
 
     /* dependency-cruiser will include modules matching against the focus
-       regular expression in its output, as well as their direct neighbours 
+       regular expression in its output, as well as their direct neighbours
        (dependencies and dependents)
     */
     // focus : '',
@@ -226,15 +226,15 @@ module.exports = {
        module systems it knows of. It's the default because it's the safe option
        It might come at a performance penalty, though.
        moduleSystems: ['amd', 'cjs', 'es6', 'tsd']
-      
+
        As in practice only commonjs ('cjs') and ecmascript modules ('es6')
        are widely used, you can limit the moduleSystems to those.
      */
-    
+
     // moduleSystems: ['cjs', 'es6'],
 
     /* prefix for links in html and svg output (e.g. 'https://github.com/you/yourrepo/blob/develop/'
-       to open it on your online repo or `vscode://file/${process.cwd()}/` to 
+       to open it on your online repo or `vscode://file/${process.cwd()}/` to
        open it in visual studio code),
      */
     // prefix: `vscode://file/${process.cwd()}/`,
@@ -244,7 +244,7 @@ module.exports = {
        "specify": for each dependency identify whether it only exists before compilation or also after
      */
     // tsPreCompilationDeps: false,
-    
+
     /* list of extensions to scan that aren't javascript or compile-to-javascript.
        Empty by default. Only put extensions in here that you want to take into
        account that are _not_ parsable.
@@ -302,7 +302,7 @@ module.exports = {
        a hack.
     */
     // exoticRequireStrings: [],
-    
+
     /* options to pass on to enhanced-resolve, the package dependency-cruiser
        uses to resolve module references to disk. The values below should be
        suitable for most situations
@@ -311,7 +311,7 @@ module.exports = {
        there will override the ones specified here.
      */
     enhancedResolveOptions: {
-      /* What to consider as an 'exports' field in package.jsons */ 
+      /* What to consider as an 'exports' field in package.jsons */
       exportsFields: ["exports"],
       /* List of conditions to check for in the exports field.
          Only works when the 'exportsFields' array is non-empty.
@@ -332,8 +332,8 @@ module.exports = {
          A list of alias fields in package.jsons
          See [this specification](https://github.com/defunctzombie/package-browser-field-spec) and
          the [resolve.alias](https://webpack.js.org/configuration/resolve/#resolvealiasfields)
-         documentation in the webpack docs. 
-         
+         documentation in the webpack docs.
+
          Defaults to an empty array (don't use any alias fields).
        */
       // aliasFields: ["browser"],
