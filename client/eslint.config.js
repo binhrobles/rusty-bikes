2a7c6f0e71d12cc19044b09264ee0852e1e49e72
Binh 🌱 <binhrobles@gmail.com>
2024-05-23 21:06:13 -0400

    migrate

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,29 +1,20 @@
 import globals from 'globals';
-
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
-import pluginJs from '@eslint/js';
-
-// mimic CommonJS variables -- not needed if using CommonJS
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: pluginJs.configs.recommended,
-});
+import stylistic from '@stylistic/eslint-plugin';
+import prettierConfig from 'eslint-config-prettier';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default [
-  ...compat.config({
-    extends: ['airbnb-base'],
-    rules: {
-      'no-console': 'off',
-    },
-    ignorePatterns: ['rainbowvis.js'],
-  }),
   {
-    languageOptions: {
-      globals: { ...globals.browser, ...globals.node },
+    ignores: ['dist/'],
+  },
+  {
+    plugins: {
+      '@stylistic': stylistic,
     },
   },
+  {
+    languageOptions: { globals: globals.browser },
+  },
+  prettierConfig, // Turns off all ESLint rules that have the potential to interfere with Prettier rules.
+  eslintPluginPrettierRecommended,
 ];
