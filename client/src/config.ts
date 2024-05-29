50dc709c58f659e1e96b96d9c3a18fb0fba5a680
Binh 🌱 <binhrobles@gmail.com>
2024-05-28 21:37:23 -0400

    dev / prod config

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,1 +1,3 @@
-export const RUSTY_BASE_URL = 'http://localhost:9000/lambda-url/lambda-handler';
+export const RUSTY_BASE_URL = import.meta.env.PROD ?
+  'https://77oxuxdr7h.execute-api.us-east-1.amazonaws.com/Prod/' :
+  'http://localhost:9000/lambda-url/lambda-handler';
