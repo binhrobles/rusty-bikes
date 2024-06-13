827dd8b52915505c9d17539e336393f4eef89492
Binh 🌱 <binhrobles@gmail.com>
2024-06-12 22:42:05 -0400

    simple lambda cold start ping

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
+
   import { createMap, configureBindings } from './modules/map.mts';
   import mode_control from './modules/mode_control.mts';
   import cost_control from './modules/cost_control.mts';
+  import { RUSTY_BASE_URL } from './config.ts';
 
   let map;
   const mapAction = (container) => {
@@ -18,6 +21,16 @@
       }
     };
   }
+
+  let lambdaReady = false;
+  onMount(async () => {
+    try {
+      const res = await fetch(`${RUSTY_BASE_URL}/ping`);
+      lambdaReady = true;
+    } catch (e) {
+      console.error(e);
+    }
+  });
 </script>
 
 <style>
@@ -32,4 +45,8 @@
   <span>Follow dev </span> <a href="https://github.com/binhrobles/rusty-bikes">here</a>
 </header>
 <br>
+{#if lambdaReady}
 <div id="map" use:mapAction></div>
+{:else}
+<span>Loading...</span>
+{/if}
